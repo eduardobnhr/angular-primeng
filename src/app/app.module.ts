575a00ca8ffc0614745133bc72758c258f26622b
eduardobnhr <eduardobnhr@gmail.com>
2024-03-05 22:19:24 -0300

    criando pagina de login faltando a imagem de fundo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardModule } from 'primeng/card';
+import { LoginComponent } from './views/login/login.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    CardModule
+    CardModule,
+    InputTextModule,
+    ButtonModule
 
   ],
   providers: [],
